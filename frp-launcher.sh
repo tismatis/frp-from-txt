b790f5a2bd0a63d687e68ee36863c1abd11d31cf
Matis André <45696697+tismatis@users.noreply.github.com>
2024-08-25 22:18:13 +0200

    Update frp-launcher.sh

--- frp-launcher.sh
+++ frp-launcher.sh
@@ -4,8 +4,8 @@
 # The format of the port mapping file is protocol,targetIp,targetPort,remotePort
 
 SCRIPT_DIR="$(dirname "$(realpath "$0")")"
-FRPC_CONFIG_FILE="$SCRIPT_DIR/frp/config/frpc_multi_proxies.toml"
-FRPC_PATH="$SCRIPT_DIR/frp/frpc"
+FRPC_CONFIG_FILE="$SCRIPT_DIR/config/frpc_multi_proxies.toml"
+FRPC_PATH="$SCRIPT_DIR/frpc"
 PORT_MAPPINGS_FILE=""
 SERVER_IP=""
 SERVER_PORT=""
